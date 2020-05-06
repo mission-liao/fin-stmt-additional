c7bb54569862d82ccdcf3ff124d474bff99294ca
MissionL <mission@byzantine-lab.io>
2020-05-06 17:06:42 +0800

    Tmp

--- fstmt/table/__init__.py
+++ fstmt/table/__init__.py
@@ -4,3 +4,4 @@ from .cost_breakdown import CostBreakdown
 from .manufacturing_expense import ManufacturingExpense
 from .stock import Stock
 from .factory import TableAdaptorFactory
+from .me import ManufacturingExpense2
